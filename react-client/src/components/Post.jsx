ad4d7a10d7824e52ddae0b000beeecf1eb5a0532
HamdiBenkahla <68249436+HamdiBenkahla@users.noreply.github.com>
2020-10-16 11:58:56 +0200

    work progress

--- react-client/src/components/Post.jsx
+++ react-client/src/components/Post.jsx
@@ -11,6 +11,7 @@ return (
   <div className="post">
     <h1 className="post-title">{this.props.anime.title}</h1>
     <i className="fa fa-thumbs-up">  </i>{this.props.anime.like} <i className="fa fa-thumbs-down"></i>  {this.props.anime.dislike}
+    <br></br>
     <img src={this.props.anime.imageUrl}/>
     <div>{this.props.anime.description}</div>
   </div>
